2c7c0efc02fbf6a890a4387c16e95f30da67417e
fless-lab <achilleatarmla@gmail.com>
2022-03-13 15:06:30 +0000

    Finalisation de l'app

---
+++ lib/home.dart
@@ -0,0 +1,96 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var controller = TextEditingController();
+  var _qrcode;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("QrCode Maker"),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 30),
+            child: Card(
+              child: ListTile(
+                leading: Icon(Icons.qr_code),
+                title: TextField(
+                  controller: controller,
+                  decoration: InputDecoration(
+                      hintText: 'Entrer une url ou un texte',
+                      border: InputBorder.none),
+                ),
+                trailing: IconButton(
+                  icon: Icon(Icons.arrow_forward),
+                  onPressed: () => getQRcode(controller.text),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 60, vertical: 50),
+            width: 300,
+            height: 300,
+            child: _buildQRcode(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void getQRcode(String content) async {
+    if (content.isEmpty) {
+      return;
+    }
+
+    //Send « to encode » string and get the name of the qrcode
+    final http.Response response = await http.post(
+        Uri.parse("http://10.0.2.2:8000/qrcode"),
+        body: jsonEncode(content));
+    final decodedResponse = json.decode(response.body);
+
+    //qrcode final link
+    final _qrcodeURL = "http://10.0.2.2:8000/get_qrcode?name=$decodedResponse";
+
+    //Build Qrcode
+    final ByteData imageData =
+        await NetworkAssetBundle(Uri.parse(_qrcodeURL)).load("");
+    final Uint8List bytes = imageData.buffer.asUint8List();
+    setState(() {
+      _qrcode = bytes;
+    });
+  }
+
+  Widget _buildQRcode() {
+    if (_qrcode == null) {
+      return Card(
+          child: Center(
+        child: Text(
+          "N/A",
+          style: TextStyle(fontSize: 30, color: Colors.grey),
+        ),
+      ));
+    }
+    return Image.memory(
+      _qrcode,
+      fit: BoxFit.cover,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -14,62 +16,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
       ),
       home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var controller = TextEditingController();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("QrCode Maker"),
-        centerTitle: true,
-      ),
-      body: ListView(
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 30),
-            child: Card(
-              child: ListTile(
-                leading: Icon(Icons.qr_code),
-                title: TextField(
-                  controller: controller,
-                  decoration: InputDecoration(
-                      hintText: 'Entrer une url ou un texte',
-                      border: InputBorder.none),
-                ),
-                trailing: IconButton(
-                  icon: Icon(Icons.arrow_forward),
-                  onPressed: () {},
-                ),
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 15, vertical: 50),
-            width: 300,
-            height: 300,
-            child: Card(
-                child: Center(
-              child: Text(
-                "N/A",
-                style: TextStyle(fontSize: 30, color: Colors.grey),
-              ),
-            )),
-          ),
-        ],
-      ),
     );
   }
 }
